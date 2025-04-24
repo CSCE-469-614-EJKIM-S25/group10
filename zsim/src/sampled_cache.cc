e70a1169c5d4f57cde4b166864054185c2b1e000
mathimalarm <mathimalar@tamu.edu>
2025-04-24 22:38:35 +0530

    Update sampled_cache.cc

--- zsim/src/sampled_cache.cc
+++ zsim/src/sampled_cache.cc
@@ -1,9 +1,7 @@
-struct SampledCacheLine {
+struct SampledCacheEntry {
     bool valid;
     uint64_t tag;
-    uint64_t signature;
+    uint64_t last_pc_signature;
     int timestamp;
 };
-unordered_map<uint32_t, SampledCacheLine* > sampled_cache;
-
-
+unordered_map<uint32_t, SampledCacheEntry* > sampled_cache;
